c11f19abeae38a87aca858fdf14d68376586123b
douglasb99 <douglas.burkinshaw@fetch.ai>
2020-06-03 13:36:39 +0100

    cors issue

--- imports/ui/home/token-tap/tokenTap.jsx
+++ imports/ui/home/token-tap/tokenTap.jsx
@@ -18,13 +18,6 @@ async function postData(url = '', data = {}) {
 
   const response = await fetch(url, {
    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    mode: 'cors', // no-cors, *cors, same-origin
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'same-origin', // include, *same-origin, omit
-    headers: {
-      'Content-Type': 'application/json'
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
     body: JSON.stringify(data)
   });
 
