c0d0b03e61e70f222a1387c2170ab80d27cde031
dougb99 <douglaswburkinshaw@hotmail.co.uk>
2020-11-19 11:01:47 +0000

    deleted old DKG stuff

--- imports/ui/blocks/Block.jsx
+++ imports/ui/blocks/Block.jsx
@@ -11,7 +11,6 @@ import i18n from 'meteor/universe:i18n';
 import TimeStamp from '../components/TimeStamp.jsx';
 import Tooltip from '@material-ui/core/Tooltip/Tooltip'
 
-
 /**
  * Get substring of address with three appended in middle of  dots; for UI display
  *
@@ -20,6 +19,7 @@ import Tooltip from '@material-ui/core/Tooltip/Tooltip'
  * @returns {string} formatted string
  * @param dots how many dots in middle of string
  */
+
 function format(val, number = 12, dots = 5) {
   if (val === '') return ''
   return (
@@ -81,13 +81,7 @@ export default class Block extends Component{
      *
      */
     showDKGInfo(){
-
-      console.log(" this.state?.DKG?.groupSignature" ,  this.props.block?.dkg?.groupSignature)
-      console.log("this.props.blockExist" ,  this.props.blockExist)
-debugger;
-         const ret = Boolean(Meteor.settings.public.DKGTab && this.props.blockExist && this.props.block.dkg?.groupSignature)
-            console.log(" showDKGInfo" ,  ret)
-return ret;
+         return Boolean(Meteor.settings.public.DKGTab && this.props.blockExist && this.props.block.dkg?.groupSignature)
     }
 
 
@@ -118,46 +112,57 @@ return ret;
                     <h4><T>blocks.block</T> {numbro(block.height).format("0,0")}</h4>
                     <Card>
                         <div className="card-header"><T>common.information</T></div>
-                        <CardBody>
-                            <Row>
-                                <Col md={4} className="label"><T>common.hash</T></Col>
-                                <Col md={8} className="value text-nowrap overflow-auto address">{block.hash}</Col>
-                                <Col md={4} className="label"><T>blocks.proposer</T></Col>
-                                <Col md={8} className="value"><Link to={"/validator/"+((proposer)?proposer.operator_address:'')}><Avatar moniker={moniker} profileUrl={profileUrl} address={block.proposerAddress} list={true} /> {moniker}</Link></Col>
-                                <Col md={4} className="label"><T>blocks.numOfTransactions</T></Col>
-                                <Col md={8} className="value">{numbro(block.transNum).format("0,0")}</Col>
-                                <Col md={4} className="label"><T>common.time</T></Col>
-                                <Col md={8} className="value"><TimeStamp time={block.time}/> ({moment(block.time).fromNow()})</Col>
-                                    {
-                                        this.showDKGInfo() ?
-                                          <>
-                                            <hr  className={"DKG-value-hr"}></hr>
-                                         <Col md={4} className="label DKG-list-header"><T>DKG.entropy</T></Col>
-                                         <Col md={8} className="value text-nowrap overflow-auto address"></Col>
+                      <CardBody>
+                        <Row>
+                          <Col md={4} className="label"><T>common.hash</T></Col>
+                          <Col md={8} className="value text-nowrap overflow-auto address">{block.hash}</Col>
+                          <Col md={4} className="label"><T>blocks.proposer</T></Col>
+                          <Col md={8} className="value"><Link
+                            to={'/validator/' + ((proposer) ? proposer.operator_address : '')}><Avatar moniker={moniker}
+                                                                                                       profileUrl={profileUrl}
+                                                                                                       address={block.proposerAddress}
+                                                                                                       list={true}/> {moniker}
+                          </Link></Col>
+                          <Col md={4} className="label"><T>blocks.numOfTransactions</T></Col>
+                          <Col md={8} className="value">{numbro(block.transNum).format('0,0')}</Col>
+                          <Col md={4} className="label"><T>common.time</T></Col>
+                          <Col md={8} className="value"><TimeStamp time={block.time}/> ({moment(block.time).fromNow()})</Col>
+                          {
+                            this.showDKGInfo() ?
+                              <>
+                                <hr className={'DKG-value-hr'}></hr>
+                                <Col md={4} className="label DKG-list-header"><T>DKG.entropy</T></Col>
+                                <Col md={8} className="value text-nowrap overflow-auto address"></Col>
+                                <Col md={4} className="label" onClick={this.copy}
+                                     data-tooltip-positions="bottom;left;top;right"><span
+                                  className={'DKG-value'}><T>DKG.groupSignature</T></span></Col>
+                                <Col md={8}  data-tooltip-positions="bottom;left;top;right"
+                                     className="value text-nowrap overflow-auto address">
+                                  {format(block.dkg.groupSignature, 30)}
+                                  <Tooltip title={this.state.copied ? <span className="toolTip">Copied!</span> : <span
+                                    className="toolTip">Copy to Clipboard</span>}>
+                                    <img onClick={this.copy} onClick src="/img/clipboard.svg" className="clipboard"/></Tooltip>
 
-                                          <Col md={4} className="label" onClick={this.copy} data-tooltip-positions="bottom;left;top;right"><span className={"DKG-value"}><T>DKG.groupSignature</T></span></Col>
-
-                                           {/*<Tooltip title={this.state.copied? <span className="toolTip">Copied!</span> : <span className="toolTip">Copy to Clipboard</span>}>*/}
-                                           <Col md={8} onClick={this.copy} data-tooltip-positions="bottom;left;top;right" className="value text-nowrap overflow-auto address">
-                                             {format(block.dkg.groupSignature, 30)}
-                                           <img src="/img/clipboard.svg" className="clipboard"/>
-                                           </Col>
-
-                                           {/*</Tooltip>*/}
-                                          <Col md={4} className="label" onClick={this.copy} data-tooltip-positions="bottom;left;top;right"><span className={"DKG-value"}><T>DKG.round</T></span></Col>
-                                           <Col md={8} className="value text-nowrap overflow-auto address">{block.dkg.round}</Col>
-
-                                          <Col md={4} className="label" onClick={this.copy} data-tooltip-positions="bottom;left;top;right"><span className={"DKG-value"}><T>DKG.startBlock</T></span></Col>
-                                           <Col md={8} className="value text-nowrap overflow-auto address">{block.dkg.startBlock}</Col>
-
-                                          <Col md={4} className="label" onClick={this.copy} data-tooltip-positions="bottom;left;top;right"><span className={"DKG-value"}><T>DKG.endBlock</T></span></Col>
-                                           <Col md={8} className="value text-nowrap overflow-auto address">{block.dkg.endBlock}</Col>
-
-                                      </>
-                                          : ""
-                                    }
-                            </Row>
-                        </CardBody>
+                                </Col>
+                                <Col md={4} className="label" onClick={this.copy}
+                                     data-tooltip-positions="bottom;left;top;right"><span
+                                  className={'DKG-value'}><T>DKG.round</T></span></Col>
+                                <Col md={8} className="value text-nowrap overflow-auto address">{block.dkg.round}</Col>
+                                <Col md={4} className="label" onClick={this.copy}
+                                     data-tooltip-positions="bottom;left;top;right"><span
+                                  className={'DKG-value'}><T>DKG.startBlock</T></span></Col>
+                                <Col md={8}
+                                     className="value text-nowrap overflow-auto address">{block.dkg.startBlock}</Col>
+                                <Col md={4} className="label" onClick={this.copy}
+                                     data-tooltip-positions="bottom;left;top;right"><span
+                                  className={'DKG-value'}><T>DKG.endBlock</T></span></Col>
+                                <Col md={8}
+                                     className="value text-nowrap overflow-auto address">{block.dkg.endBlock}</Col>
+                              </>
+                              : ''
+                          }
+                        </Row>
+                      </CardBody>
                     </Card>
                     <TranactionTabs
                         transferTxs={this.state.transferTxs}
@@ -165,7 +170,6 @@ return ret;
                         distributionTxs={this.state.distributionTxs}
                         governanceTxs={this.state.governanceTxs}
                         slashingTxs={this.state.slashingTxs}
-                        DKG={block.dkg}
                     />
                 </Container>
             }
