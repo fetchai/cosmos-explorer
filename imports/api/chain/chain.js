e60ad26119ffc3500792a8876e1ac95b8b088e82
daeMOn <flavien.binet@gmail.com>
2021-04-13 16:38:48 +0000

    fix: wrong fees format, use plain afet instead of scientific notation fet (#78)

--- imports/api/chain/chain.js
+++ imports/api/chain/chain.js
@@ -4,8 +4,6 @@ import { Validators } from '../validators/validators.js';
 export const Chain = new Mongo.Collection('chain');
 export const ChainStates = new Mongo.Collection('chain_states');
 
-console.log("DID GET TO STARTING THESE COLLECTIONS")
-
 Chain.helpers({
   proposer() {
     return Validators.findOne({ address: this.proposerAddress });
