9b948908b1c2c1bf466608560c409d3f5f9537d5
Flavien Binet <flavien.binet@gmail.com>
2021-04-15 15:01:19 +0200

    fix: adjust page titles

--- imports/ui/validators/ValidatorsList.jsx
+++ imports/ui/validators/ValidatorsList.jsx
@@ -83,8 +83,8 @@ export default class Validators extends Component {
 
     return <div id="validator-list">
       <Helmet>
-        <title>Cosmos Validator List | The Big Dipper</title>
-        <meta name="description" content="Here is a list of Cosmos Validators" />
+        <title>The Fetch.ai Network Explorer</title>
+        <meta name="description" content="Here is a list of Fetch.ai Validators" />
       </Helmet>
       <Row>
         <Col lg={3} xs={12}><h1 className="d-none d-lg-block">{title}</h1></Col>
