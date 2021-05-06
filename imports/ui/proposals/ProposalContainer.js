fd2a78c90de52d5febaa07e92da9e38eda7c7787
Flavien Binet <flavien.binet@gmail.com>
2021-05-06 14:46:36 +0200

    fix: proposals page throws 'Invalid prop component of type object supplied to Route, expected function'

--- imports/ui/proposals/ProposalContainer.js
+++ imports/ui/proposals/ProposalContainer.js
@@ -3,42 +3,48 @@ import { withTracker } from 'meteor/react-meteor-data';
 import { Proposals } from '/imports/api/proposals/proposals.js';
 import { Chain } from '/imports/api/chain/chain.js';
 import Proposal from './Proposal.jsx';
+import { Component } from 'react';
 
-export default ProposalContainer = withTracker((props) => {
-  let proposalId = 0;
-  if (props.match.params.id) {
-    proposalId = parseInt(props.match.params.id);
+export default class ProposalContainer extends Component {
+
+  render() {
+    return withTracker((props) => {
+      let proposalId = 0;
+      if (props.match.params.id) {
+        proposalId = parseInt(props.match.params.id);
+      }
+
+      let chainHandle; let proposalHandle; let proposalListHandle; let proposal; let proposalCount; let chain; let
+        proposalExist;
+      let loading = true;
+
+      if (Meteor.isClient) {
+        chainHandle = Meteor.subscribe('chain.status');
+        proposalListHandle = Meteor.subscribe('proposals.list', proposalId);
+        proposalHandle = Meteor.subscribe('proposals.one', proposalId);
+        loading = !proposalHandle.ready() || !chainHandle.ready() || !proposalListHandle.ready();
+      }
+
+      if (Meteor.isServer || !loading) {
+        proposal = Proposals.findOne({ proposalId });
+        proposalCount = Proposals.find({}).count();
+        chain = Chain.findOne({ chainId: Meteor.settings.public.chainId });
+
+        if (Meteor.isServer) {
+          // loading = false;
+          proposalExist = !!proposal;
+        } else {
+          proposalExist = !loading && !!proposal;
+        }
+      }
+
+      return {
+        loading,
+        proposalExist,
+        proposal: proposalExist ? proposal : {},
+        chain: proposalExist ? chain : {},
+        proposalCount: proposalExist ? proposalCount : 0,
+      };
+    })
   }
-
-  let chainHandle; let proposalHandle; let proposalListHandle; let proposal; let proposalCount; let chain; let
-    proposalExist;
-  let loading = true;
-
-  if (Meteor.isClient) {
-    chainHandle = Meteor.subscribe('chain.status');
-    proposalListHandle = Meteor.subscribe('proposals.list', proposalId);
-    proposalHandle = Meteor.subscribe('proposals.one', proposalId);
-    loading = !proposalHandle.ready() || !chainHandle.ready() || !proposalListHandle.ready();
-  }
-
-  if (Meteor.isServer || !loading) {
-    proposal = Proposals.findOne({ proposalId });
-    proposalCount = Proposals.find({}).count();
-    chain = Chain.findOne({ chainId: Meteor.settings.public.chainId });
-
-    if (Meteor.isServer) {
-      // loading = false;
-      proposalExist = !!proposal;
-    } else {
-      proposalExist = !loading && !!proposal;
-    }
-  }
-
-  return {
-    loading,
-    proposalExist,
-    proposal: proposalExist ? proposal : {},
-    chain: proposalExist ? chain : {},
-    proposalCount: proposalExist ? proposalCount : 0,
-  };
-})(Proposal);
+}
