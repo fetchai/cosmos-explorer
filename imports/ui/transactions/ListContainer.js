0f0ec0605c9e83c466f5c510b485a997dd4e59f9
Flavien Binet <flavien.binet@gmail.com>
2021-05-07 12:00:27 +0200

    fix: transactions list fully refresh when reaching bottom of page

--- imports/ui/transactions/ListContainer.js
+++ imports/ui/transactions/ListContainer.js
@@ -4,25 +4,24 @@ import { Transactions } from '/imports/api/transactions/transactions.js';
 import List from './List.jsx';
 
 export default ValidatorDetailsContainer = withTracker((props) => {
-  let transactionsHandle; let transactions; let
-    transactionsExist;
-  let loading = true;
+    let transactionsHandle; let transactions; let
+        transactionsExist;
+    let loading = true;
 
-    if (Meteor.isClient){
+    if (Meteor.isClient) {
         transactionsHandle = Meteor.subscribe('transactions.list', props.limit);
-        loading = !transactionsHandle.ready();
-
+        loading = (!transactionsHandle.ready() && props.limit == Meteor.settings.public.initialPageSize);
         if (!loading) {
-            transactions = Transactions.find({}, {sort:{height:-1}}).fetch();
+            transactions = Transactions.find({}, { sort: { height: -1 } }).fetch();
             transactionsExist = !loading && !!transactions;
         }
     }
 
-    if (Meteor.isServer){
-        transactions = Transactions.find({}, {sort:{height:-1}}).fetch();
+    if (Meteor.isServer) {
+        transactions = Transactions.find({}, { sort: { height: -1 } }).fetch();
         transactionsExist = !!transactions;
     }
-    
+
     return {
         loading,
         transactionsExist,
