f06cc05e7d3471df5637d10b6d28af64925f5bd7
Flavien Binet <flavien.binet@gmail.com>
2021-05-07 11:52:29 +0200

    fix: limit coin decimals

--- both/utils/coins.js
+++ both/utils/coins.js
@@ -28,8 +28,6 @@ export default class Coin {
         // divider used to convert between denom or displayName representation
         this._coin.fraction = Big(this._coin.fraction)
         // threshold used to switch display of a coin between its denom or displayName representations.
-        // when amount is below this threshold, denom value will be used
-        // when above, it will be converted to displayName.
         this._fractionDisplayThreshold = this._coin.fraction.div(Big(1000000));
 
         if (!amount) {
@@ -50,11 +48,18 @@ export default class Coin {
     toString() {
         let amount = this._amount;
         let denom = this._coin.denom;
+        // when amount is below this threshold, denom value will be used
+        // when above, it will be converted to displayName.
         if (this._amount.gt(this._fractionDisplayThreshold)) {
             amount = amount.div(this._coin.fraction);
             denom = this._coin.displayName;
         }
 
-        return `${numbro(amount.toString()).format("0,0")} ${denom}`
+        let format = '0,0.000000'
+        // removes unecessary decimals
+        if (amount.eq(amount.round(0, Big.roundDown))) {
+            format = '0,0'
+        }
+        return `${numbro(amount.toString()).format(format)} ${denom}`
     }
 }
