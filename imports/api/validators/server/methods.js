6fe842a42b8f337f0ba8bc4947ca3947d95e868b
Flavien Binet <flavien.binet@gmail.com>
2021-07-20 17:34:02 +0200

    fix: retrieve full list of delegations (with pagination)

--- imports/api/validators/server/methods.js
+++ imports/api/validators/server/methods.js
@@ -27,21 +27,32 @@ Meteor.methods({
     },
     'Validators.getAllDelegations'(address) {
         this.unblock();
-        let url = `${API}/cosmos/staking/v1beta1/validators/${address}/delegations`;
 
-        try {
-            let delegations = HTTP.get(url);
-            if (delegations.statusCode == 200) {
-                delegations = JSON.parse(delegations.content).delegation_responses;
+        let allDelegations = [];
+        const limit = 100;
+        let offset = 0;
+        let total = 0;
+        do {
+            let url = `${API}/cosmos/staking/v1beta1/validators/${address}/delegations?pagination.limit=${limit}&pagination.offset=${offset}&pagination.count_total=1`;
+            try {
+                const resp = HTTP.get(url);
+                const response = JSON.parse(resp.content);
+                total = parseInt(response.pagination.total);
+                let delegations = response.delegation_responses;
                 delegations.forEach((_, i) => {
                     if (delegations[i] && delegations[i].shares)
                         delegations[i].shares = parseFloat(delegations[i].shares);
                 })
-                return delegations;
+                allDelegations = [...allDelegations, ...delegations];
+                offset += limit;
+            } catch (e) {
+                console.log(url);
+                console.log("Getting error: %o when getting delegations from %o", e, url);
+                return [];
             }
-        } catch (e) {
-            console.log(url);
-            console.log("Getting error: %o when getting delegations from %o", e, url);
         }
+        while (allDelegations.length < total);
+
+        return allDelegations;
     }
 });
