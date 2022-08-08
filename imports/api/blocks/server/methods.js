705422f228903e5433c921ab10c6bf3f0fe8507c
Ian Harris <ian.harris@fetch.ai>
2022-08-08 11:15:04 +0100

    fix: eridanus block data

--- imports/api/blocks/server/methods.js
+++ imports/api/blocks/server/methods.js
@@ -240,7 +240,7 @@ Meteor.methods({
                 this.unblock();
                 // let url = RPC+'/block?height=' + height;
 
-                url = `${API}/cosmos/base/tendermint/v1beta/blocks/${height}`;
+                url = `${API}/cosmos/base/tendermint/v1beta1/blocks/${height}`;
                 let analyticsData = {};
 
                 const bulkValidators = Validators.rawCollection().initializeUnorderedBulkOp();
